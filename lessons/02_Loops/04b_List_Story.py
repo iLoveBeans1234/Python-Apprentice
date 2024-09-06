91b2dbded40b1115b32911bdbc16fd4818b463a6
Matthew5129 <132800763+Matthew5129@users.noreply.github.com>
2024-09-06 23:51:30 +0000

    I did 2 projects

--- lessons/02_Loops/04b_List_Story.py
+++ lessons/02_Loops/04b_List_Story.py
@@ -7,11 +7,11 @@ append them to the story
 """
 
 words = ['Once', '👦', 'upon', '🐕', 'park', 'met', 'with', 'a', 'the', 
-    'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 'they']
+    'time', 'to', 'who', '🐈', 'and', 'went', 'had', 'play', 'they']
 
 story = []
 
 # Create a story using the words in the list
-['Once upon a time, a dog met a boy at the park. They played together. But one day a cat went to the park. Dog went to the cat and told cat to leave but cat does not leave.']
+['Once upon a time a dog met a boy at the park. They played together. A cat went to the park. Dog went to the cat and boy went to the cat']
 # Display the story to the user
 print(' '.join(story))
